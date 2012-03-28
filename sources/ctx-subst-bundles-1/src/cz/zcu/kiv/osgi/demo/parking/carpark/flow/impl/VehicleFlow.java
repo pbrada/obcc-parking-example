807e1f1d7fab96215d18f5f7856cb8d0aad10b94
Premek Brada <brada@kiv.zcu.cz>
2012-03-29 00:31:15 +0200

    first modifications towards svc-oriented impl, in carpark and gate - impl classes

---
+++ sources/ctx-subst-bundles-1/src/cz/zcu/kiv/osgi/demo/parking/carpark/flow/impl/VehicleFlow.java
@@ -0,0 +1,58 @@
+package cz.zcu.kiv.osgi.demo.parking.carpark.flow.impl;
+
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cz.zcu.kiv.osgi.demo.parking.carpark.flow.IVehicleFlow;
+import cz.zcu.kiv.osgi.demo.parking.carpark.status.impl.ParkingStatus;
+
+
+public class VehicleFlow implements IVehicleFlow
+{
+	private static VehicleFlow instance = null;
+	private Logger logger = null;
+	
+	private ParkingStatus status = null;
+	private int capacity = 0;
+		
+
+	public static VehicleFlow getInstance()
+	{
+		if (instance == null) {
+			instance = new VehicleFlow();
+		}
+		return instance;		
+	}
+
+	
+	protected VehicleFlow()
+	{
+		this.logger = LoggerFactory.getLogger("parking-demo");
+		logger.info("VehicleFlow.r1 <init>");
+		this.status = ParkingStatus.getInstance();
+		this.capacity = 10;
+	}	
+
+	
+	@Override
+	public void arrive()
+	{
+		logger.info("VehicleFlow: arrive");
+		if (--capacity <= 0) {
+			capacity = 0;
+			status.setFull(true);
+		}
+	}
+
+	@Override
+	public void leave()
+	{
+		logger.info("VehicleFlow: leave");
+		// we don't check reaching max capacity for now
+		if (++capacity > 0) {
+			status.setFull(false);
+		}
+	}
+
+}
---
+++ sources/ctx-subst-bundles-1/src/cz/zcu/kiv/osgi/demo/parking/carpark/status/impl/ParkingStatus.java
@@ -0,0 +1,46 @@
+package cz.zcu.kiv.osgi.demo.parking.carpark.status.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cz.zcu.kiv.osgi.demo.parking.carpark.status.IParkingStatus;
+
+
+
+public class ParkingStatus implements IParkingStatus
+{
+
+	private static ParkingStatus instance = null;
+	private Logger logger = null;
+	
+	private boolean isFull = false;
+	
+
+	public static ParkingStatus getInstance() 
+	{
+		if (instance == null) {
+			instance = new ParkingStatus();
+		}
+		return instance;
+	}
+	
+	
+	protected ParkingStatus()
+	{
+		this.logger = LoggerFactory.getLogger("parking-demo");
+		logger.info("ParkingStatus.r1 <init>");
+	}	
+
+	@Override
+	public boolean isFull()
+	{
+		logger.info("ParkingStatus: isFull {}", isFull);
+		return isFull;
+	}
+	
+	public void setFull(boolean status)
+	{
+		this.isFull = status;
+	}
+
+}
